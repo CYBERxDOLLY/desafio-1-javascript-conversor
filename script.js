1c26249903576ca822089b0080a9e8b65543f3a7
CYBERxDOLLY <brayansoaresdeaguiar@gmail.com>
2023-12-13 17:06:09 -0300

    API para valores dinâmicos adicionada ao código

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const convertButton = document.querySelector(".convert-button") // mapear meu bo
 const currencySelect = document.querySelector(".currency-select") // mapear meu select
 
 // função principal para converter as moedas
-function convertValues(){
+async function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value // mapear o valor do meu input
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //  valor em real
     const currencyValueConverted = document.querySelector(".currency-value") // outras moedas
-    const dolarToday = 5.2 // armazenar o valor do dólar
-    const euroToday = 6.2
-    const britishPoundToday = 6.8
-    const bitcoinToday = 34.04
 
-    if(currencySelect.value == "dolar"){
+
+    //API para valores dinâmicos de moeda
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,BTC-BRL").then(response => response.json())
+
+
+    const dolarToday = data.USDBRL.high
+    const euroToday = data.EURBRL.high
+    const britishPoundToday = data.GBPBRL.high
+    const bitcoinToday = data.BTCBRL.high
+
+
+    if (currencySelect.value == "dolar") {
         // configuração para formatar os números corretamente
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -19,21 +26,21 @@ function convertValues(){
         }).format(inputCurrencyValue / dolarToday) // () divide o valor inserido no input pelo valor do dólar e etc
     }
 
-    if(currencySelect.value == "euro"){
+    if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
     }
 
-    if(currencySelect.value == "libra"){
+    if (currencySelect.value == "libra") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "GBP"
         }).format(inputCurrencyValue / britishPoundToday)
     }
 
-    if(currencySelect.value == "bitcoin"){
+    if (currencySelect.value == "bitcoin") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en", {
             style: "currency",
             currency: "BTC"
@@ -47,26 +54,26 @@ function convertValues(){
 }
 
 // função para mudar o nome e a bandeira das moedas
-function changeCurrency(){
+function changeCurrency() {
     const currencyName = document.getElementById("currency-name") // armazena o nome da moeda
     const currencyImage = document.querySelector(".currency-img") // armazena a imagem da moeda
-    
-    if(currencySelect.value == "dolar") {
+
+    if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar americano" // troca o nome da moeda
         currencyImage.src = "./assets/img/dolar.png" // troca a imagem da moeda
     }
-    
-    if(currencySelect.value == "euro") {
+
+    if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/img/euro.png"
     }
 
-    if(currencySelect.value == "libra") {
+    if (currencySelect.value == "libra") {
         currencyName.innerHTML = "Libra"
         currencyImage.src = "./assets/img/libra.png"
     }
 
-    if(currencySelect.value == "bitcoin") {
+    if (currencySelect.value == "bitcoin") {
         currencyName.innerHTML = "Bitcoin"
         currencyImage.src = "./assets/img/bitcoin.png"
     }
